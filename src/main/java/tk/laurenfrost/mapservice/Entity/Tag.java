f6ae7692e271b26683fe219701ec02c0cad8f95d
almdudleer <36701344+almdudleer@users.noreply.github.com>
2020-02-08 10:59:07 +0300

    Tag mapped

--- src/main/java/tk/laurenfrost/mapservice/Entity/Tag.java
+++ src/main/java/tk/laurenfrost/mapservice/Entity/Tag.java
@@ -1,4 +1,17 @@
 package tk.laurenfrost.mapservice.Entity;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class Tag {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
 }
